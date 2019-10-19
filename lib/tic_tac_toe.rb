5d29b25216fbec7a13ed8197faf7568f84f95d9f
Mark Kauffman <markkauffman22@gmail.com>
2019-10-19 02:30:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ class TicTacToe
   
   def valid_move(i)
     @position_taken = position_taken?(i)
-    return (!position_taken) && (i>0 && i<8)
+    return (!@position_taken) && (i>0 && i<8)
   end  
   
   def current_player
