0aa6c9d975012643e2243b33f3ba019e2c39130b
Mark Kauffman <markkauffman22@gmail.com>
2019-10-19 03:46:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,6 +93,17 @@ class TicTacToe
     end
   end
  
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+ 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
