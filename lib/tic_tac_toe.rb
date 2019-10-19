b7a0483c281f52fc6199a15c7b7488a8eb197c08
Mark Kauffman <markkauffman22@gmail.com>
2019-10-19 03:22:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,11 @@ class TicTacToe
   end  
   
   def draw?
-    
+    if full?(board)
+      return true if !(won?(board)) && full?(board)
+      return false if !(won?(board)) && !(full?(board))
+      return false if won?(board)
+    end 
   end  
  
  
