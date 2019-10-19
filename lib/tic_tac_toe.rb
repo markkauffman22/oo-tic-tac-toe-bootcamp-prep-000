c7d102771c726b9e205b2187fede13b572e037ec
Mark Kauffman <markkauffman22@gmail.com>
2019-10-19 03:10:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -53,7 +53,7 @@ class TicTacToe
       input = gets.strip
       index = input_to_index(input)
       char =current_player
-      if valid_move(index)
+      if valid_move?(index)
          move(index, char)
          display_board
       else
